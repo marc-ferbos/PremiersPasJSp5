ac3c35c4ab281240f044cf44b95779dca7a50d5f
Marc <marc.ferbos@gmail.com>
2023-09-10 20:27:14 +0200

    Modif terminées

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,96 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+let currentSlide = 0;
+
+const leftArrow = document.querySelector('.arrow_left');
+const rightArrow = document.querySelector('.arrow_right');
+const bannerImage = document.querySelector('.banner-img');
+let tagLineElement = document.querySelector('.tagline');
+
+
+leftArrow.addEventListener('click', function () {
+	if (currentSlide === 0) {
+		currentSlide = slides.length - 1;
+	} else {
+		currentSlide--;
+	}
+	updateSlide();
+	
+	/*currentSlide = ( currentSlide === 0 ) ? slides.length - 1 : currentSlide - 1;*/
+	/*currentSlide = ( currentSlide -1 + slides.length ) % slides.length;*/
+	/*document.querySelector('.tagLine').innerHTML = slides[currentSlide].tagLine;*/
+});
+
+rightArrow.addEventListener('click', function () {
+	if (currentSlide === (slides.length - 1)) {
+		currentSlide = 0;
+	} else {
+		currentSlide++;
+	}
+	updateSlide();
+
+});
+
+/* Création fonction pour mettre à jour les slides */
+
+function updateSlide() {
+	tagLineElement.innerHTML = slides[currentSlide].tagLine;
+	bannerImage.src = 'assets/images/slideshow/' + slides[currentSlide].image + '' ;
+	console.log(slides[currentSlide].tagLine);
+	updateDots();
+}
+/* Création fonction des dots */
+
+function createDots() {
+	const dots = document.querySelector('.dots');
+	for (let i = 0; i < slides.length; i++) {
+		const dot = document.createElement('div');
+		if (i === 0) {
+			dot.classList.add('dot_selected');
+		}
+		dot.classList.add('dot');
+		dot.addEventListener('click', function () {
+			currentSlide = i;
+			updateSlide();
+		});
+		dots.appendChild(dot);
+	}
+}
+createDots();
+
+
+/* Création fonction mettre à jour les dots */
+
+function updateDots() {
+	const dots = document.querySelectorAll('.dot');
+	dots.forEach(function (dot) {
+		dot.classList.remove('dot_selected');
+	});
+	dots[currentSlide].classList.add('dot_selected');
+}
+
+/* Création fonction pour faire défiler les slides automatiquement */
+
+function autoSlide() {
+	setInterval(function () {
+		if (currentSlide === (slides.length - 1)) {
+			currentSlide = 0;
+		} else {
+			currentSlide++;
+		}
+		updateSlide();
+	}, 5000);
+}
+autoSlide();
+
+
+
+
+
+
+
+
+
+
